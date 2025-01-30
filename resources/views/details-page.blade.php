46bc43affbeffa165830614ad0c40d087a095465
Supun Nethsara <52463797+Snrox@users.noreply.github.com>
2025-01-30 13:59:05 +0530

    Update details-page.blade.php

--- resources/views/details-page.blade.php
+++ resources/views/details-page.blade.php
@@ -53,7 +53,7 @@
 
                 <br>
                 @if($gem->gallery_images && count($gem->gallery_images) > 0)
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-3 border border-metal-700 rounded-lg p-3">
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-3 border border-metal-700 rounded-lg p-3">
 
 
                     @foreach($gem->gallery_images as $image)
